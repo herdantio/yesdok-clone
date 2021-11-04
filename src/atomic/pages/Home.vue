ca1ba5d5010f3e53aa2a17a7c4dabc31aad8d86b
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-11-04 08:05:54 +0700

    Install & implement bootstrap-vue Implement atomic component design Add navbar Remove unused component Refactor routing Add pages

---
+++ src/atomic/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav-bar/>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {
+    NavBar: () => import('@/atomic/organisms/NavBar')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
